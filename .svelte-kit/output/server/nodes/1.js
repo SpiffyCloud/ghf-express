6322fb2a0b3e63e6cf8b16f3d7bef4ff143255a3
Alex Payne <iamalexpayne360@gmail.com>
2024-10-04 15:35:36 -0400

    alex overhaul with svelte

---
+++ .svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.D7NVY_df.js","_app/immutable/chunks/disclose-version.DsA8IxDv.js","_app/immutable/chunks/runtime.aMMvh1H4.js","_app/immutable/chunks/render.Z6BS0m-B.js","_app/immutable/chunks/entry.Arf63M3D.js"];
+export const stylesheets = [];
+export const fonts = [];
