6cf7787af293a6410f6a8c9b0e07a33bdd6cc97c
Dayron Edmundo Acosta <dayroned@gmail.com>
2024-10-13 12:37:06 -0400

    Delete build artifacts and temporal files

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.DJC8Ofo7.js","_app/immutable/chunks/2.z7wPYXzY.js","_app/immutable/chunks/disclose-version.DsA8IxDv.js","_app/immutable/chunks/runtime.aMMvh1H4.js","_app/immutable/chunks/render.Z6BS0m-B.js","_app/immutable/chunks/preload-helper.DU57GtT2.js"];
-export const stylesheets = ["_app/immutable/assets/2.CvIcALUl.css"];
-export const fonts = [];
